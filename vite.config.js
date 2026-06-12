5f4331505062a1a84c846ccf3f88be6092730909
Famine von Valfoutre <antro920@gmail.com>
2026-06-12 10:47:51 +0700

    configured vite config file

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/naufal-pasya-aldri1-26/",  // <-- ADD THIS EXACT LINE
+  base: "/Task-Tracker/",  // <-- CHANGE THIS LINE TO MATCH YOUR
 })
